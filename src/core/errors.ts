379259d6127bd1f44dee963d78e1ac8f7343ece9
Keiver-Dev <keiverlunaps11@gmail.com>
2026-04-03 15:18:56 -0300

    feat(errors): add NodulusErrorCode union type

--- src/core/errors.ts
+++ src/core/errors.ts
@@ -1,3 +1,14 @@
+export type NodulusErrorCode =
+  | 'MODULE_NOT_FOUND'
+  | 'DUPLICATE_MODULE'
+  | 'MISSING_IMPORT'
+  | 'UNDECLARED_IMPORT'
+  | 'CIRCULAR_DEPENDENCY'
+  | 'EXPORT_MISMATCH'
+  | 'INVALID_CONTROLLER'
+  | 'ALIAS_NOT_FOUND'
+  | 'DUPLICATE_BOOTSTRAP'
+
 export class NodulusError extends Error {
   code: string;
 
